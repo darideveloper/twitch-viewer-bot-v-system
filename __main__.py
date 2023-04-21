a30d16d0595639d787e8cc828a71b21e0324c0c6
darideveloper <darideveloper@gmail.com>
2023-04-21 01:44:37 -0600

    feat (main): run bots with main file

--- __main__.py
+++ __main__.py
@@ -1,23 +1,5 @@
-# import api
-# from bots import ViwerBot
-# from threading import Thread
+from bots import BotsManager
 
-# def main (): 
-        
-#     # # Get active users
-#     # active_users = api.get_active_users()
-    
-#     # # Start scrapers and crate chrome data folders
-#     # threads = []
-#     # for user, password in active_users.items():
-#     #     twitch_bot = ViwerBot (user, password, "https://www.twitch.tv/juansguarnizo")
-#     #     threads.append(Thread (target=twitch_bot.auto_run))
-        
-#     # # Start threads
-#     # for thread in threads:
-#     #     thread.start()
-    
-#     ViwerBot ()
-
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    BotsManager()
+    input ("\nPress any key to exit...")
