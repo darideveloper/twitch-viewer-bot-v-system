93231b06676058f3a9491a5e41a7890ee17d90ec
daridev <darideveloper@gmail.com>
2023-08-21 14:55:14 -0600

    feat (api): update endpoints for twitch_v_system

--- api.py
+++ api.py
@@ -1,131 +1,120 @@
 import os
+import json
+import random
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 API_HOST = os.getenv("API_HOST")
-TOKEN = os.getenv("TOKEN")
+TOKEN_WEBSHARE = os.getenv("TOKEN_WEBSHARE")
+TOKEN_VIWERS = os.getenv("TOKEN_VIWERS")
+TOKEN_STREAMS = os.getenv("TOKEN_STREAMS")
 
+LOGS_PREFIX = "(api)"
 
 class Api ():
-
-    def __requests_url__(self, endpoint: str) -> requests.get:
-        """ Request data from specific endpoint and and quit if error happens
-
-        Args:
-            endpoint (str): endpoint to request, like "users" or "settings"
-
-        Returns:
-            requests.get: response of requests to the endpoint
+    
+    def __init__ (self):
+        self.proxies = []
+        
+        self.__load_proxies__ ()     
+        
+    def __load_proxies__ (self):
+        """ Query proxies from the webshare api, and save them
         """
+        
+        print (f"{LOGS_PREFIX} Loading proxies...")
+        
+        # Get proxies
+        res = requests.get (
+            "https://proxy.webshare.io/api/v2/proxy/list/?mode=direct&page=1&page_size=100", 
+            headers = { 
+                "Authorization": f"Token {TOKEN_WEBSHARE}"
+            }
+        )
+        if res.status_code != 200:
+            print (f"Error getting proxies: {res.status_code} - {res.text}")
+            quit ()
 
-        # Request data to specific url
-        url = f"{API_HOST}/{endpoint}/?token={TOKEN}"
-        res = requests.get(url)
-
-        if res.status_code == 200:
-            return res
-        else:
-            print("Error requesting data from API. Check your token.")
-            quit()
-
-    def get_users(self) -> list:
-        """ Get twitch user for the bot, using the API
+        try:
+            json_data = res.json ()
+            self.proxies = json_data['results']
+        except Exception as error:
+            print (f"{LOGS_PREFIX} Error getting proxies: {error}")
+            quit ()
+            
+    def get_users (self) -> list:
+        """ users and passwords from the API
 
         Returns:
-            list: list of dictionaries with user data
-
-            Example: 
-            [
+            dict: user data (id, username and password)
+            
+            Example:
+            [    
                 {
-                    "name": "DariDeveloper",
-                    "cookies": [...],
-                    "is_active": True,
-                    "last_update": "2023-04-17T23:29:07.083Z"
+                    "id": 1,
+                    "user": "sample 1 user",
+                    "cookies": {"hello": "word"},
+                },
+                {
+                    "id": 2,
+                    "user": "sample 2 user",
+                    "cookies": {"hello": "word"},
                 }
-                ...
             ]
+
         """
-
-        print("getting users...")
-
+        
+        print (f"{LOGS_PREFIX} Getting users...")
+        
         # Get data from api
-        res = self.__requests_url__("users")
-        users = res.json()
-
-        # filter active users
-        users = list(filter(lambda user: user["fields"]["is_active"], users))
-
-        # Format users
-        users = list(map(lambda user: user["fields"], users))
-
+        res = requests.get (
+            f"{API_HOST}/viwers/bots/", 
+            headers={"token": TOKEN_VIWERS}
+        )
+        res.raise_for_status ()
+        json_data = res.json ()
+        
+        # Validate response
+        if json_data["status"] != "ok":
+            print (f"{LOGS_PREFIX} Error getting users: {json_data['message']}")
+            quit ()
+        
+        users = json_data["data"]
+        
+        # Filter only active users
+        users = list(filter (lambda user: user["is_active"], users))
+        
+        # Formatd ata
+        users = list(map (lambda user: {
+            "id": user["id"],
+            "user": user["user"],
+            "cookies": user["cookies"],
+        }, users))
+        
         return users
 
-    def get_settings(self) -> list:
-        """ Get settings for the bot, using the API
+    def get_proxy (self) -> dict:
+        """ get a random proxy 
 
         Returns:
-            list: dictionary with settings data
-
-            Example: {"viwers_stream": 20}
-        """
-
-        print("getting settings...")
-
-        # Get data from api
-        res = res = self.__requests_url__("settings")
-        settings = res.json()
-
-        # Format settings
-        setting_formatted = {}
-        for setting in settings:
-            key = setting["fields"]["name"]
-            value = setting["fields"]["value"].lower().strip()
-
-            # Convert values to int if possible
-            if value.isdigit():
-                value = int(value)
-
-            # Convert to bool if is possible
-            if value == "true":
-                value = True
-
-            if value == "false":
-                value = False
-
-            setting_formatted[key] = value
-
-        return setting_formatted
-
-    def get_proxies(self) -> list:
-        """ Get proxies for the bot, using the API
-
-        Returns:
-            list: list of dictionaries with proxy data
-
+            dict: proxy data (host and port)
+            
             Example:
-            [
-                {
-                    "host": "123.123.123.123",
-                    "port": 80,
-                    "user": "my user",
-                    "password": "my password",
-                    "location": 1
-                }
-                ...
-            ]
+            {
+                "host": "0.0.0.0",
+                "port": 80,
+            }
         """
-
-        print("getting proxies...")
-
+        
+        print (f"\t{LOGS_PREFIX} Getting a random proxy...")
+        
         # Get data from api
-        res = res = self.__requests_url__("proxies")
-        proxies = res.json()
-
-        # Format proxies
-        proxies = list(map(lambda proxy: proxy["fields"], proxies))
-
-        return proxies
+        proxy = random.choice (self.proxies)
+        return {
+            "host": proxy["proxy_address"],
+            "port": proxy["port"],
+        }
 
     def get_streams(self) -> list:
         """ Get current live streams in comunidad mc, using the API
@@ -136,40 +125,60 @@ class Api ():
             Example:  ["DariDeveloper", "darideveloper2"]
         """
 
-        print("getting streams...")
-
-        # Debug
-        # return ["darideveloper"]
-
-        # Get data from api
-        res = self.__requests_url__("streams")
-        return res.json()
+        print(f"{LOGS_PREFIX} Getting streams...")
         
+        # Get data from api
+        res = requests.get (
+            f"{API_HOST}/streams/current-streams",
+            headers={"token": TOKEN_STREAMS}
+        )
+        json_data = res.json()
+        streams = json_data["data"]
+        
+        return streams
     
-    def disable_user (self, username:str): 
+    def disable_user (self, user_id:int, user_name:str): 
         """ Disable user in the API
 
         Args:
-            username (str): user name to disable
+            user_id (int): user id
+            user_name (str): user name
         """
         
-        res = self.__requests_url__("disable-user/" + username)
-        print (f"\t{username}: {res.text}")
+        print (f"{LOGS_PREFIX} Disabling user {user_name}...")
         
-    def log_error (self, error:str):
-        """ Log error in the API
+        res = requests.delete (
+            f"{API_HOST}/viwers/bots/",
+            headers={"token": TOKEN_VIWERS},
+            json={"id": user_id},
+        )
+        
+        json_data = res.json ()
+        if json_data["status"] != "ok":
+            print (f"{LOGS_PREFIX} Error disabling user: {json_data['message']}")
+        
+        
+    # def log_error (self, error:str):
+    #     """ Log error in the API
 
-        Args:
-            error (str): error message
-        """
+    #     Args:
+    #         error (str): error message
+    #     """
         
-        url = f"{API_HOST}/log-error/?token={TOKEN}"
-        try:
-            res = requests.post (url, json={"error": error})
-            res.raise_for_status()
-        except:
-            print ("Error saving error to API")
-    
+    #     url = f"{API_HOST}/log-error/?token={TOKEN}"
+    #     try:
+    #         res = requests.post (url, json={"error": error})
+    #         res.raise_for_status()
+    #     except:
+    #         print ("Error saving error to API")
+        
 if __name__ == "__main__":
-    api = Api()
-    api.disable_user("vegetta_pelon")
+    
+    api = Api ()
+    users = api.get_users ()
+    proxy = api.get_proxy ()
+    streams = api.get_streams ()
+    disabled_user = random.choice (users)
+    api.disable_user (disabled_user["id"], disabled_user["user"])
+    
+    print ()
